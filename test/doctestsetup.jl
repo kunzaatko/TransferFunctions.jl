82f0b95f2bd618979265374e6d4b97d5b6601944
Martin Kunz <martinkunz@email.cz>
2025-07-29 22:45:24 +0200

    fix(test): Do not load MakieMaestro in the doctest setup script

--- test/doctestsetup.jl
+++ test/doctestsetup.jl
@@ -1,7 +1,6 @@
 using TransferFunctions
 using TransferFunctions.Apodization
 using TestImages
-using MakieMaestro
 filenames = ["moonsurface.tiff"]; # NOTE: This is a fix for failing doctests since on download, there is a print-out <19-12-24> 
 testimage.(filenames; download_only=false);
 using TransferFunctions: FFTW;
