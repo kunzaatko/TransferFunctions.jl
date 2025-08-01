ac604b14a867f8650deec8fd17dc095666cf3d10
Martin Kunz <martinkunz@email.cz>
2025-08-01 14:53:10 +0200

    feat: Make Estimation.bead return a SpatialArray

--- src/psf/estimation/beads-acquisition.jl
+++ src/psf/estimation/beads-acquisition.jl
@@ -2,7 +2,7 @@ using Unitful
 using Unitful: Length, Quantity, 𝐋
 using OffsetArrays: OffsetMatrix, OffsetArray
 using Statistics
-using TransferFunctions: PixelSize
+using TransferFunctions: PixelSize, SpatialArray
 const PerLength = Quantity{<:Any,inv(𝐋)}
 
 """
@@ -24,7 +24,7 @@ julia> Estimation.bead(95u"nm", (30.5u"nm", 25u"nm"));
 julia> Estimation.bead(Float32, 0.1u"μm", 30.5u"nm");
 
 julia> Estimation.bead(0.1u"μm", 30.5u"nm"; α=0.01u"nm^-1")
-5×5 OffsetArray(::Matrix{Float64}, -2:2, -2:2) with eltype Float64 with indices -2:2×-2:2:
+5×5 SpatialArray{Float64, 2, Quantity{Float64, 𝐋, Unitful.FreeUnits{(nm,), 𝐋, nothing}}, OffsetArrays.OffsetMatrix{Float64, Matrix{Float64}}} with indices -2:2×-2:2:
  0.0        0.0       0.0705075  0.0       0.0
  0.0        0.606779  0.892914   0.606779  0.0
  0.0705075  0.892914  1.0        0.892914  0.0705075
@@ -32,13 +32,13 @@ julia> Estimation.bead(0.1u"μm", 30.5u"nm"; α=0.01u"nm^-1")
  0.0        0.0       0.0705075  0.0       0.0
 
 julia> Estimation.bead(0.1u"μm", 50.5u"nm"; position=(-0.3,-0.2))
-3×3 OffsetArray(::Matrix{Float64}, -1:1, -1:1) with eltype Float64 with indices -1:1×-1:1:
+3×3 SpatialArray{Float64, 2, Quantity{Float64, 𝐋, Unitful.FreeUnits{(nm,), 𝐋, nothing}}, OffsetArrays.OffsetMatrix{Float64, Matrix{Float64}}} with indices -1:1×-1:1:
  0.333333  0.737374  0.0808081
  0.59596   1.0       0.20202
  0.020202  0.141414  0.0
 
 julia> Estimation.bead(0.1u"μm", 50.5u"nm"; intensity=0.5)
-3×3 OffsetArray(::Matrix{Float64}, -1:1, -1:1) with eltype Float64 with indices -1:1×-1:1:
+3×3 SpatialArray{Float64, 2, Quantity{Float64, 𝐋, Unitful.FreeUnits{(nm,), 𝐋, nothing}}, OffsetArrays.OffsetMatrix{Float64, Matrix{Float64}}} with indices -1:1×-1:1:
  0.03  0.23  0.03
  0.23  0.5   0.23
  0.03  0.23  0.03
@@ -52,7 +52,7 @@ function bead(
     pixel_grid=10,
     intensity=one(T),
     position::Tuple{Real,Real}=(0.0, 0.0)
-)::OffsetMatrix{T}
+)
     @assert all(abs.(position) .< one(eltype(position))) "|position| must be < 1"
     subpx_pad = map(position) do x
         (x < zero(x) ? x : 0, x > zero(x) ? x : 0)  # padding to apply to the buffer to fit the sub-pixel shift
@@ -61,7 +61,9 @@ function bead(
         round.(Int, (ax_range .+ pad), RoundFromZero) # range of the axes in px including the sub-pixel padding
     end
 
-    grid = Matrix{T}(undef, (((b - a + 1) * pixel_grid) for (a, b) in buf_axes_px)...)
+    grid_size = (((b - a + 1) * pixel_grid) for (a, b) in buf_axes_px)
+    @assert all(>(0), grid_size) LazyString("Invalid grid size: $(collect(grid_size)). The problem may be in the combination of `d` and `Δxy` arguments such that the bead does not span a single pixel in some dimension.")
+    grid = Matrix{T}(undef, grid_size...)
     grid_subpixel_shift = position .* pixel_grid
     grid_center = ((1 / 2 .- first.(buf_axes_px)) .* pixel_grid) .+ grid_subpixel_shift
 
@@ -85,7 +87,7 @@ function bead(
     end
     buf = OffsetArray(buf, (first.(buf_axes_px) .- 1)...)
     buf .*= intensity / buf[0, 0]
-    return buf
+    return SpatialArray(buf, Δxy)
 end
 # NOTE: step 1 add default type
 bead(d, Δxy; vargs...) = bead(Float64, d, Δxy; vargs...)
