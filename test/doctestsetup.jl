7037c97609a8b9c00613ace507c15e072753e835
Martin Kunz <martinkunz@email.cz>
2025-08-12 01:24:35 +0200

    fix: return nothing from `doctestsetup.jl` to avoid clutter

--- test/doctestsetup.jl
+++ test/doctestsetup.jl
@@ -9,3 +9,5 @@ using FFTW
 using TestImages
 filenames = ["moonsurface.tiff"]; # NOTE: This is a fix for failing doctests since on download, there is a print-out <19-12-24> 
 testimage.(filenames; download_only=false);
+
+return nothing
