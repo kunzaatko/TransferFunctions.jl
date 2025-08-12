5aa573b719a537fd44d5111eeed385235ad4aace
Martin Kunz <martinkunz@email.cz>
2025-08-12 03:33:17 +0200

    fix(test): Set the `LINES` and `COLUMNS` for doctests

--- test/doctestsetup.jl
+++ test/doctestsetup.jl
@@ -6,6 +6,9 @@ using OffsetArrays: OffsetArrays as OAs
 
 using FFTW
 
+ENV["COLUMNS"] = 100
+ENV["LINES"] = 80
+
 using TestImages
 filenames = ["moonsurface.tiff"]; # NOTE: This is a fix for failing doctests since on download, there is a print-out <19-12-24> 
 testimage.(filenames; download_only=false);
