bb42f07c504f370122b7cd8f7e9a3007ade42832
Martin Kunz <martinkunz@email.cz>
2025-08-13 19:03:47 +0200

    feat(extensions): Add method for `scalebar` to sampled matrices

--- ext/MakieExt/MakieExt.jl
+++ ext/MakieExt/MakieExt.jl
@@ -2,12 +2,17 @@ module MakieExt
 using Makie
 using TransferFunctions
 using TransferFunctions: Apodization as Apo
-using TransferFunctions: SpatialMatrix
+using TransferFunctions: SampledMatrix, sampling
 import TransferFunctions: scalebar, scalebar!, scalebarformat
 
-Makie.convert_arguments(::PointBased, fn::Apo.ApodizationFunction)  = (-1..1,x -> Apo.apodization(fn, x))
-Makie.convert_arguments(::ImageLike, A::SpatialMatrix)  = (parent(A),)
-
 include("MakieExt_scalebar.jl")
+
+Makie.convert_arguments(::PointBased, fn::Apo.ApodizationFunction)  = (-1..1,x -> Apo.apodization(fn, x))
+Makie.convert_arguments(::ImageLike, A::SampledMatrix)  = (parent(A),)
+function Makie.convert_arguments(::Type{<:Scalebar}, A::SampledMatrix) 
+    scale = sampling(A)
+    @assert allequal(scale) "Sampling must be isotropic otherwise a `scalebar` is not well defined."
+    return (first(scale),)
+end
 
 end # end module
