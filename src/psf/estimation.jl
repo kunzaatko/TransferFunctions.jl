64d1fa5bdf4d0de7ca1267ba02b1d7b68d3a7548
Martin Kunz <martinkunz@email.cz>
2025-08-26 13:01:44 +0200

    feat: Add estimation routine for least squares

--- src/psf/estimation.jl
+++ src/psf/estimation.jl
@@ -1,12 +1,19 @@
 module Estimation
+using Base: Indices
 using InterfaceFunctions
-using TransferFunctions: Size, FilteringMatrix, aroundorigin, PSFModel, TransferFunction, PSFArray
+using TransferFunctions
+using TransferFunctions: Size, aroundorigin, PSFModel, TransferFunction, PSFArray, sampling, inner_axes, PSFArray
+using OffsetArrays: no_offset_view
 include("estimation/beads-acquisition.jl")
 
 abstract type EstimationAlgorithm{H<:TransferFunction} end
 abstract type FromGroundTruth{H<:TransferFunction} <: EstimationAlgorithm{H} end
 
 # TODO: Add subtypes and output type from estimation to the required interface <06-05-25> 
+"""
+    estimate(model::Estimation.FromGroundTruth, raw::SpatialArray, gt::SpatialArray)
+Estimate the PSF from the ground truth `gt` and transferred data `raw`.
+"""
 @interface estimate(model::Estimation.FromGroundTruth, raw::AbstractArray, gt::AbstractArray)
 
 # TODO: Should not hold the size but instead the indices that are used for the construction of the `FilteringMatrix` <05-05-25> 
@@ -16,23 +23,22 @@ abstract type FromGroundTruth{H<:TransferFunction} <: EstimationAlgorithm{H} end
 # Fields
 - `size::Size{2}` -- size of the PSF to estimate
 """
-struct LeastSquares <: FromGroundTruth{PSFArray}
-    size::Size{2}
+struct LeastSquares{N, I<:Indices{N}} <: FromGroundTruth{PSFArray{N}}
+    indices::I
+    LeastSquares(inds::Indices) = new{length(inds), typeof(inds)}(inds)
+end
+LeastSquares(size::Size) = LeastSquares(aroundorigin(size))
+
+function estimate(ls::LeastSquares{N}, raw::SpatialArray{<:Any,N}, gt::SpatialArray{<:Any,N}) where {N}
+    @assert sampling(raw) == sampling(gt) "sampling rates of `raw` and `gt` must match"
+    F_A = filtering_matrix(reflect(gt), ls.indices)
+    B = raw[inner_axes(raw, ls.indices)...]
+    LhS_op = F_A * no_offset_view(reflect(F_A'))
+    RhS = F_A * B[:]
+    h_vec =  LhS_op \ RhS
+    return PSFArray(SampledArray(reshape(h_vec, ls.indices), sampling(raw)))
 end
 
-# TODO: Define for SpatialArrays and check the sampling rates of `gt` and `raw` <05-05-25> 
-# TODO: Add padding options <05-05-25> 
-function estimate(ls::LeastSquares, raw::AbstractMatrix{T}, gt::AbstractMatrix{T}) where {T<:Real}
-    A = FilteringMatrix(gt, aroundorigin(ls.size))
-    h_vec = (A' * raw[:]) \ (A * A')
-    return reshape(h_vec, A.Kaxes)
-end
-
-abstract type Optimization end
-
-struct ForwardPassInversion{M<:PSFModel,O<:Optimization} <: FromGroundTruth{M}
-end
-
-abstract type BlindDeconvolution{H<:TransferFunction} <: EstimationAlgorithm{H} end
+export LeastSquares
 
 end
--- test/estimation.jl
+++ test/estimation.jl
@@ -1,4 +1,7 @@
+using TransferFunctions: reflect
 using TransferFunctions.Estimation
+using TestImages
+using OffsetArrays: no_offset_view
 
 pixel_grid = 3 # PERF: Smaller grid for decreasing the test time
 
@@ -8,3 +11,47 @@ pixel_grid = 3 # PERF: Smaller grid for decreasing the test time
 
 @test (bead(100u"nm", 30.5u"nm"; position=(0.5, 0.5), pixel_grid) .== reverse(bead(100u"nm", 30.5u"nm"; position=(-0.5, -0.5), pixel_grid))) |> all
 @test (2bead(100u"nm", 30.5u"nm"; intensity=0.5, pixel_grid) .== bead(100u"nm", 30.5u"nm"; pixel_grid)) |> all
+
+@testset "LeastSquares" begin
+  @testset "theory" begin
+    tf = AiryDisc{2}(488u"nm", 1.4)
+    gt = SpatialMatrix(Float32.(TestImages.testimage("mandril_gray"))[begin:5:end, begin:5:end], 64u"nm")
+    indices = (-5:5, -5:5)
+    psf_matrix = psf(tf, 64u"nm", (length.(indices)))
+    raw = TF.conv(gt, psf_matrix)
+
+    F_A = filtering_matrix(reflect(gt), indices)
+    B = raw[TF.inner_axes(raw, indices)...]
+    B_from_A = reshape(reflect(F_A' * psf_matrix[:]), axes(B))
+    @test isapprox(B_from_A, B, rtol=1e-3)
+
+    LhS = F_A * no_offset_view(reflect(F_A')) * psf_matrix[:]
+    RhS = F_A * B[:]
+    @test isapprox(LhS, RhS, rtol=1e-3)
+
+    psf_estim = (F_A * no_offset_view(reflect(F_A'))) \ RhS
+    @test isapprox(psf_matrix[:], psf_estim[:], rtol=1e-1)
+  end
+
+  @testset "estimation" begin
+    using Test, Distributions
+    tf = AiryDisc{2}(488u"nm", 1.4)
+    bs = SyntheticData.beads(300, 100u"nm", (64u"nm", 64u"nm"), (256, 256))
+    gt = SyntheticData.groundtruth(bs)
+    ls = TF.Estimation.LeastSquares((-5:5, -5:5))
+    raw = conv(gt, psf(tf, 64u"nm", (length.(ls.indices))))
+    psf_estim = TF.Estimation.estimate(ls, raw, gt)
+    psf_true = psf(tf, 64u"nm", (length.(ls.indices)))
+    @test parent(psf_estim) ≈ psf_true
+
+    # raw_noisy_1 = similar(raw)
+    # raw_noisy_1 .= raw .+ 0.01*randn(size(raw)) / sum(raw.^2)
+    # raw_noisy_2 = similar(raw)
+    # raw_noisy_2 .= raw .+ 0.05*randn(size(raw)) / sum(raw.^2)
+    # raw_noisy_3 = similar(raw)
+    # raw_noisy_3 .= raw .+ 0.1*randn(size(raw)) / sum(raw.^2)
+    # psf_estim_noisy_1 = TF.Estimation.estimate(ls, raw_noisy_1, gt)
+    # psf_estim_noisy_2 = TF.Estimation.estimate(ls, raw_noisy_2, gt)
+    # psf_estim_noisy_3 = TF.Estimation.estimate(ls, raw_noisy_3, gt)
+  end
+end
