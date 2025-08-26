c3a286ee5f11e9c3374c65cb9b152a7913180e0b
Martin Kunz <martinkunz@email.cz>
2025-08-26 12:41:04 +0200

    feat: Use `posaxes` in the plotting of SpatialMatrix

--- ext/MakieExt/MakieExt.jl
+++ ext/MakieExt/MakieExt.jl
@@ -4,11 +4,12 @@ using TransferFunctions
 using TransferFunctions: Apodization as Apo
 using TransferFunctions: SampledMatrix, sampling
 import TransferFunctions: scalebar, scalebar!, scalebarformat
+using TransferFunctions: TransferFunctions as TF
 
 include("MakieExt_scalebar.jl")
 
 Makie.convert_arguments(::PointBased, fn::Apo.ApodizationFunction)  = (-1..1,x -> Apo.apodization(fn, x))
-Makie.convert_arguments(::ImageLike, A::SampledMatrix)  = (parent(A),)
+Makie.convert_arguments(::ImageLike, A::SpatialMatrix)  = (TF.posaxes(A)..., parent(A),)
 function Makie.convert_arguments(::Type{<:Scalebar}, A::SampledMatrix) 
     scale = sampling(A)
     @assert allequal(scale) "Sampling must be isotropic otherwise a `scalebar` is not well defined."
