1dd1a304e653988026d3f3ba4c4ff07144b98344
Martin Kunz <martinkunz@email.cz>
2025-08-26 12:49:51 +0200

    style: Format, use utility functions for checking and add dimensionality parameter to BornWolf

--- src/psf/types/airy-disc.jl
+++ src/psf/types/airy-disc.jl
@@ -7,48 +7,68 @@ Airy disc scalar diffraction point spread function model for a circular aperture
 - `NA::Real`: numerical aperture of the objective
 - `n::Real`: refractive index of the immersion medium (defaults to `4//3` which is the refractive index of water)
 """
-struct AiryDisc{N, T<:Real} <: PSFModel{N}
+struct AiryDisc{N,T<:Real} <: PSFModel{N}
     λ::Length{T}
     NA::T
     n::T # refractive index of water
-    function AiryDisc{N, T}(λ::Length{T}, NA::T, n::T) where {N,T}
+    function AiryDisc{N,T}(λ::Length{T}, NA::T, n::T) where {N,T}
         N ∈ (2, 3) || throw(ArgumentError("`AiryDisc` model can only be used for 2D and 3D PSFs. Got `N == $N`."))
         check_emission_wavelength(λ)
         check_numerical_aperture(NA)
         check_refractive_index(n)
-        return new{N, T}(λ, NA, n)
+        return new{N,T}(λ, NA, n)
     end
 end
-AiryDisc{N}(;λ, NA, n = 4 // 3) where {N} = AiryDisc{N}(λ, NA, n)
-AiryDisc{N}(λ::Length, NA::Real; kwargs...) where {N} = AiryDisc{N}(;λ, NA, kwargs...)
-AiryDisc(args...;kwargs...) = AiryDisc{3}(args...;kwargs...)
+AiryDisc{N}(; λ, NA, n=float(4 // 3)) where {N} = AiryDisc{N}(λ, NA, n)
+AiryDisc{N}(λ::Length, NA::Real; kwargs...) where {N} = AiryDisc{N}(; λ, NA, kwargs...)
+AiryDisc(args...; kwargs...) = AiryDisc{3}(args...; kwargs...) # default to the 3D full model
 
 """
     AiryDisc{N}(λ::Length, NA, n=4//3)
 [`AiryDisc`](@ref) model point spread function with emission wavelength `λ`, numerical aperture `NA` and immersion
 medium refractive index `n`.
 """
-function AiryDisc{N}(λ::Length{A}, NA::Real, n::Real) where {A<:Real, N}
+function AiryDisc{N}(λ::Length{A}, NA::Real, n::Real) where {A<:Real,N}
     T = promote_type(A, typeof(NA), typeof(n))
     λ = convert(T, ustrip(λ)) * unit(λ)
     NA, n = convert(T, NA), convert(T, n)
-    return AiryDisc{N, T}(λ, NA, n) # inner
+    return AiryDisc{N,T}(λ, NA, n) # inner
 end
 
 symmetry(::AiryDisc) = ZAxisRadialSymmetry()
 
-function intensity(tf::AiryDisc{<:Any, T}, r::Length) where {T}
-  α = 2π * tf.NA / tf.λ
-  return iszero(r) ? oneunit(T) : (2 * besselj1(α * r) / (α * r))^2
+"""
+    intensity(psf::AiryDisc, r::Length)
+Compute the intensity of the [`AiryDisc`](@ref) PSF at a distance `r` from its center.
+
+Intensity of the Airy disc is given by ``h(r) = (2J₁(αr)/(αr))²``  where ``α = 2πNA/λ`` and ``J₁`` is the first order
+Bessel function of the first kind.
+"""
+function intensity(tf::AiryDisc{<:Any,T}, r::Length) where {T}
+    α = 2π * tf.NA / tf.λ
+    return iszero(r) ? oneunit(T) : (2 * besselj1(α * r) / (α * r))^2
 end
 
-function axialintensity(tf::AiryDisc{3, T}, z::Length) where {T}
-  ϕ_z = (π * tf.NA^2 * z) / (tf.λ * tf.n) # normalized axial phase
-  return iszero(z) ? oneunit(T) : sinc(ϕ_z)^2
+"""
+    axialintensity(tf::AiryDisc{3}, z::Length)
+Calculate the axial intensity of an [`AiryDisc`](@ref) PSF at the ``z``-offset `z`.
+
+Axial intensity of the Airy disc is given by ``h(r) = sinc(ϕ_z)²``  where ``ϕ_z = π NA²z/λn``.
+"""
+function axialintensity(tf::AiryDisc{3,T}, z::Length) where {T}
+    ϕ_z = (π * tf.NA^2 * z) / (tf.λ * tf.n) # normalized axial phase
+    return iszero(z) ? oneunit(T) : sinc(ϕ_z)^2
 end
 
+"""
+    intensity(tf::AiryDisc{3}, r::Length, z::Length)
+Compute the intensity of the [`AiryDisc`](@ref) PSF at a distance `r` from its center and ``z``-offset `z`.
+
+It is defined as the product of the [lateral intensity](@ref `intensity(::AiryDisc{<:Any,T}, r::Length) where {T}`) and
+the [axial intensity](@ref `axialintensity(::AiryDisc{3,T}, z::Length) where {T}`).
+"""
 function intensity(tf::AiryDisc{3}, r::Length, z::Length)
-  intensity(tf, r) * axialintensity(tf, z)
+    intensity(tf, r) * axialintensity(tf, z)
 end
 
 export AiryDisc
--- src/psf/types/born-wolf.jl
+++ src/psf/types/born-wolf.jl
@@ -1,5 +1,5 @@
 """
-    BornWolf{T<:Real} <: PSFModel{3}
+    BornWolf{N,T<:Real} <: PSFModel{N}
 Born & Wolf point spread function model.
 
 # Fields
@@ -7,35 +7,39 @@ Born & Wolf point spread function model.
 - `NA::Real`: numerical aperture of the objective
 - `n::Real`: refractive index of the immersion medium (defaults to `4//3` which is the refractive index of water)
 """
-@kwdef struct BornWolf{T<:Real} <: PSFModel{3}
+struct BornWolf{N,T<:Real} <: PSFModel{N}
     λ::Length{T}
     NA::T
     f::T
-    n::T = 4 // 3 # refractive index of water
-    function BornWolf{T}(λ::Length{T}, NA::T, f::T, n::T) where {T}
-        λ > zero(λ) || throw(DomainError(λ, "Emission wavelength is a positive value. Got `λ = $λ`."))
-        NA > zero(NA) || throw(DomainError(NA, "Numerical aperture of the objective is a positive value. Got `NA = $NA`."))
+    n::T
+    function BornWolf{N,T}(λ::Length{T}, NA::T, f::T, n::T) where {N,T}
+        check_emission_wavelength(λ)
+        check_numerical_aperture(NA)
+        check_refractive_index(n)
         f > zero(f) || throw(DomainError(n, "Refractive index of the immersion is a positive value. Got `n = $n`."))
-        n > zero(n) || throw(DomainError(n, "Refractive index of the immersion is a positive value. Got `n = $n`."))
-        return new{T}(λ, NA,f, n)
+
+        return new{N,T}(λ, NA, f, n)
     end
 end
+BornWolf{N}(;λ, f, NA, n=float(4 // 3)) where {N} = BornWolf{N}(λ,NA,f,n)
+BornWolf{N}(λ::Length, NA::Real, f::Real; kwargs...) where {N} = BornWolf{N}(; λ, f, NA, kwargs...)
+BornWolf(args...; kwargs...) = BornWolf{3}(args...; kwargs...) # default to the 3D full model
 
 """
     BornWolf(λ::Length, NA, f, n=4//3)
 [`BornWolf`](@ref) model point spread function with emission wavelength `λ`, numerical aperture `NA` and immersion medium
 refractive index `n`.
 """
-function BornWolf(λ::Length{A}, NA::Real, f::Real, n::Real) where {A<:Real}
+function BornWolf{N}(λ::Length{A}, NA::Real, f::Real, n::Real) where {N,A<:Real}
     T = promote_type(A, typeof(NA), typeof(n))
     λ = convert(T, ustrip(λ)) * unit(λ)
-    NA,f, n = convert(T, NA), convert(T,f), convert(T, n)
-    return BornWolf{T}(λ, NA,f, n)
+    NA, f, n = convert(T, NA), convert(T, f), convert(T, n)
+    return BornWolf{N,T}(λ, NA, f, n)
 end
-BornWolf(λ::Length, f::Real, NA::Real) = BornWolf(;λ,f, NA)
+
 symmetry(::BornWolf) = ZAxisRadialSymmetry()
 
-function intensity(tf::BornWolf{T}, r::Length)::T where {T}
+function intensity(tf::BornWolf{<:Any,T}, r::Length)::T where {T}
     k = 2π / tf.λ
     k₀ = k / tf.n
     return r == zero(r) ? oneunit(T) : (2besselj1(k₀ * r * tf.NA) / (k₀ * r * tf.NA))^2
