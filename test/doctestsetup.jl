8ac8372e6fcd68b0fc3ca9eb7865e88d256c384c
Martin Kunz <martinkunz@email.cz>
2025-09-03 14:20:16 +0200

    feat(test): Add `setup_makiemaestro!` and `setup_params!` as helper eval functions

--- test/doctestsetup.jl
+++ test/doctestsetup.jl
@@ -14,4 +14,25 @@ using TestImages
 filenames = ["moonsurface.tiff"]; # NOTE: This is a fix for failing doctests since on download, there is a print-out <19-12-24> 
 testimage.(filenames; download_only=false);
 
+setup_makiemaestro!() = @eval begin
+    using MakieMaestro
+    using MakieMaestro.Recipes
+    image = Recipes.image
+    image! = Recipes.image!
+    mosaic = Recipes.mosaic
+    nothing
+end
+
+setup_params!() = @eval begin
+    λ = 488u"nm"
+    NA = 1.4
+    n = 1.5
+    Δ = 64u"nm"
+    Δx = 64u"nm"
+    Δy = 32u"nm"
+    Δz = 15u"nm"
+    f = 1.3
+    nothing
+end
+
 return nothing
