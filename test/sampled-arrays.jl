b032b67f9ea0248a25fb3eec55b1e17dc53c1590
Martin Kunz <martinkunz@email.cz>
2025-09-05 11:10:15 +0200

    style: Use TF when imported as a nick for TransferFunctions

--- test/sampled-arrays.jl
+++ test/sampled-arrays.jl
@@ -13,9 +13,9 @@ end
 @test SpatialArray(ones(10, 10), (20.0u"nm", 20u"nm")) isa SampledArray{<:Any,typeof(20.0u"nm")} # Mixed types in sampling are promoted
 @test SpatialArray(ones(10, 10), (20u"nm", 2e-3u"m")) isa SpatialArray # Mixed units are promoted
 @test let s = SpatialArray(ones(10, 10), 20u"nm")
-    s isa SpatialArray && allequal(TransferFunctions.sampling(s))  # Single sampling is inferred for all dimensions
+    s isa SpatialArray && allequal(TF.sampling(s))  # Single sampling is inferred for all dimensions
 end
-@test TransferFunctions.sampling(similar(SpatialArray(ones(10, 10), (50u"nm", 50u"nm")))) == (50u"nm", 50u"nm") # `similar` preserves sampling
+@test TF.sampling(similar(SpatialArray(ones(10, 10), (50u"nm", 50u"nm")))) == (50u"nm", 50u"nm") # `similar` preserves sampling
 @test_throws MethodError SpatialArray(ones(10, 10), (20u"nm", 20u"nm", 20u"nm"))
 @test_throws MethodError SpatialArray(ones(10, 10), (20u"nm",))
 
